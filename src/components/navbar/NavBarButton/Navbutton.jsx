45276f52b1dbb653013ff494afb40b100bf32aba
Omramanuj <ramanuj.om.r@gmail.com>
2024-02-04 15:32:21 +0530

    added routes,sturcture and navbar

---
+++ src/components/navbar/NavBarButton/Navbutton.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Navbutton.css'
+
+
+export default function Navbutton({title="Button"}) {
+  return (
+    <div className='nav-button'>{title}</div>
+  )
+}
---
+++ src/pages/opp.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Opp() {
+  return (
+    <div>Opportunities</div>
+  )
+}
+
