a99c2ebe37d3fa60b7c5e224be1e28878a0cada5
ViveK Poonia <vivekpoonia2112@gmail.com>
2024-02-12 15:56:21 +0530

    Card made

--- client/src/pages/opp.jsx
+++ client/src/pages/opp.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import Footer from '../components/Footer'
+import Card from '../components/Card'
 
 export default function Opp() {
-  return (
+  return (<>
+    <Card/>
     <Footer/>
+    </>
   )
 }
 
