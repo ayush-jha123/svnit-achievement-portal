2b18724805218cf2159fd82aba21dd92fe2a5b47
brijeshpatolia <bpatolialtc@gmail.com>
2024-02-13 17:07:14 +0530

    side navbar added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home'
 import Achivements from './pages/Achivements'
 import { BrowserRouter,Route, Routes } from 'react-router-dom'
 import Opp from './pages/opp'
+import Dashboard from './pages/Dashboard'
 // import Navbar from './components/Navbar'
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Route path='/' element={<Home />}></Route>
           <Route path='/Achivements' element={<Achivements />}></Route>
           <Route path='/Opportunities' element={< Opp/>}></Route>
+          <Route path='/Dashboard' element={<Dashboard/>}></Route>
         </Routes>
     </>
     </BrowserRouter>
