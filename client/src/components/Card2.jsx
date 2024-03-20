c34f4f54cea0cdc213eb29f0028a0409e5762619
Oct-o-Dev <Abhineetk213@gmail.com>
2024-03-20 22:57:01 +0530

    ach and opp changes

--- client/src/components/Card2.jsx
+++ client/src/components/Card2.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
 import './Card2.css'
+import batman from '../../public/assets/bat.jpeg'
 
 const Card = () => {
   return (
     <div className="card">
         <div className="bar">
             <div className="photo">
+              <img
+                className='image'
+                src={batman}
+              />
             </div>
             <span>2nd Year ECE</span>
         </div>
         <div className="card_info">
-            <p>Name:</p>
-            <p>Achievement :</p>
+            <p id='font'><b>Name: </b>Vivek Lal</p>
+            <p id='font'><b>Achievement :</b> Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, doloribus! </p>
         </div>
+        <a id='view'>View more</a>
     </div>
 
   )
