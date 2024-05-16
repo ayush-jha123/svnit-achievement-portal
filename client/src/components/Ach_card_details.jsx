456caff3f5125306e4849ac348ac24e2e5e82165
AyushJha <kumarayushjha123@gmail.com>
2024-05-16 18:16:37 +0530

    some changes

--- client/src/components/Ach_card_details.jsx
+++ client/src/components/Ach_card_details.jsx
@@ -30,17 +30,17 @@ const achCardDetails = () => {
   const achievement = filterAchievements[0];
   return (
     <div>
-        <div class="max-w-md mx-auto bg-white shadow-lg rounded-lg overflow-hidden">
-    <div class="px-4 py-2">
-        <h1 class="text-2xl font-bold mb-2">Title of Achievement</h1>
-        <h3 class="text-lg font-bold mb-2">Name of user who did this</h3>
-        <p class="text-gray-600 mb-4">Description about him</p>
-        <img class="w-80 h-80 object-cover mx-auto mb-4" src={batman} alt="User Image" />
-        <p class="text-gray-600">Email of user</p>
-    </div>
+    <div className="max-w-md mx-auto bg-white shadow-lg rounded-lg overflow-hidden">
+<div className="px-4 py-2">
+    <h1 className="text-2xl font-bold mb-2">{achievement.title}</h1>
+    <h3 className="text-lg font-bold mb-2">{achievement.postedby}</h3>
+    <p className="text-gray-600 mb-4">{achievement.description}</p>
+    <img className="w-80 h-80 object-cover mx-auto mb-4" src={batman} alt="User Image" />
+    <p className="text-gray-600">Email of user</p>
+</div>
 </div>
 
-    </div>
+</div>
   )
 }
 
--- client/src/components/Opp_card_details.jsx
+++ client/src/components/Opp_card_details.jsx
@@ -28,17 +28,16 @@ const oppCardDetails = () => {
   return (
     <div class="max-w-md mx-auto bg-white shadow-lg rounded-lg overflow-hidden">
     <div class="px-4 py-2">
-        <h1 class="text-2xl font-bold mb-2">Title of Opportunity</h1>
+        <h1 class="text-2xl font-bold mb-2">{opp.title}</h1>
         <p class="text-gray-600">
-            Description about opportunity in detail<br />
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt temporibus aliquid error, quam nulla molestiae ratione officiis, quis inventore, quod illo cum voluptate iure ea. Ex explicabo odit quibusdam, est alias quas vel laboriosam nihil quos modi aliquam cumque nisi voluptatibus asperiores ab corporis, porro in hic perferendis. Doloremque fugiat quasi doloribus et tempore repellat iusto vero dolor aliquam in?
+            {opp.description}
         </p>
     </div>
     <div class="px-4 py-2">
-        <h3 class="text-lg text-blue-600">Time period/tenure</h3>
+        <h3 class="text-lg text-blue-600">Time period/tenure :{opp.openingdate} : {opp.closingdate}</h3>
     </div>
     <div class="px-4 py-2">
-        <h3 class="text-lg font-bold mb-2">Name of user who is giving this</h3>
+        <h3 class="text-lg font-bold mb-2">For Whom :{opp.participants}</h3>
         <p class="text-gray-600">Email of user</p>
     </div>
 </div>
