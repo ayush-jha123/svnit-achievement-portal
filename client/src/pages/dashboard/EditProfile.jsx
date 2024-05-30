2f0d5154672dbfd50332adbb2ab4c70283d72fd4
AyushJha <kumarayushjha123@gmail.com>
2024-05-30 17:53:22 +0530

    edit profile is functional now integrated with firebase service for image

--- client/src/pages/dashboard/EditProfile.jsx
+++ client/src/pages/dashboard/EditProfile.jsx
@@ -1,8 +1,72 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import DashSide from './DashSide.jsx';
+import { useDispatch, useSelector } from 'react-redux';
+import { signInFailure, signInSuccess } from '../../redux/user/userSlice.js';
+import { useRef } from 'react';
+import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage';
+import {app} from '../../firebase';
 
 export default function EditProfile() {
+  const fileRef=useRef();
+  const [image,setImage]=useState(undefined);
+  const [imagePercent,setImagePercent]=useState(0);
+  const [imageError,setIamgeError]=useState(false);
+  const {currentUser}=useSelector(state=>state.user);
+  const [formData,setFormData]=useState(currentUser);
+  const navigate=useNavigate();
+  console.log(formData)
+  const dispatch=useDispatch();
+  const handleChange=(e)=>{
+     setFormData({...formData,[e.target.name]:e.target.value})
+  }
+
+  useEffect(()=>{
+    if(image){
+      handleFileUpload(image);
+    }
+  },[image]);
+
+  const handleFileUpload=async (image)=>{
+    const storage=getStorage(app);
+    const fileName=new Date().getTime()+ image.name;
+    const storageRef=ref(storage,fileName);
+    const uploadTask=uploadBytesResumable(storageRef,image);
+    uploadTask.on('state_changed',(snapshot)=>{
+      const progress=(snapshot.bytesTransferred/snapshot.totalBytes)*100; 
+      setImagePercent(Math.round(progress));
+    },
+    (error)=>{
+      setIamgeError(true);
+    },
+    ()=>{
+      getDownloadURL(uploadTask.snapshot.ref).then((downloadURL)=>{
+          setFormData({...formData,profilePicture:downloadURL});
+      })
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // add proxy at vite.config
+    try {
+      // dispatch(signInStart());
+      const res=await fetch('/user/update',{
+        method:'POST',
+        headers:{
+          'Content-Type':'application/json',
+        },
+        body:JSON.stringify(formData),
+      });
+      const data=await res.json();
+      dispatch(signInSuccess(data));
+      navigate('/Dashboard')
+      
+      console.log(data);
+    } catch (error) {
+      dispatch(signInFailure(error));
+    }
+  };
   return (
     <div className="grid grid-flow-col grid-cols-auto mt-12 gap-2">
       <DashSide />
@@ -15,16 +79,34 @@ export default function EditProfile() {
           </header>
         </div>
         <form>
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="avatar">
-              Avatar
-            </label>
+          <div >
             <input
               type="file"
               id="avatar"
-              name="avatar"
+              ref={fileRef}
+              name="profilePicture"
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              onChange={e=>setImage(e.target.files[0])}
+              hidden
             />
+            <div className='flex justify-center h-[153px]'>
+            <img
+              className="icon w-36 h-36 mb-[-3.25rem] z-10 rounded-full border-4 border-white shadow-lg"
+              src={formData.profilePicture}
+              alt=""
+              style={{ width: "150px", height: "150px" }}
+              onClick={()=>fileRef.current.click()}
+            />
+            </div>
+            <p className='text-sm text-center'>
+              {imageError?(
+                <span className='text-red-700'>Error uploading image(file size must be less than 2MB)</span>
+              ):imagePercent>0 && imagePercent<100 ?(
+                  <span className='text-slate-700'>{`uploading:${imagePercent} %`}</span>
+              ):imagePercent===100?(
+                <span className='text-green-700'>Image Uploaded successfully</span>
+              ):('')}
+            </p>
           </div>
           <div className="mb-4">
             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
@@ -34,7 +116,9 @@ export default function EditProfile() {
               type="text"
               id="name"
               name="name"
+              value={formData?.name}
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              onChange={handleChange}
             />
           </div>
           <div className="mb-4">
@@ -45,7 +129,9 @@ export default function EditProfile() {
               type="text"
               id="degree"
               name="degree"
+              value={formData?.degree}
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              onChange={handleChange}
             />
           </div>
           <div className="mb-4">
@@ -56,7 +142,9 @@ export default function EditProfile() {
               type="text"
               id="department"
               name="department"
+              value={formData?.department}
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              onChange={handleChange}
             />
           </div>
           <div className="mb-4">
@@ -66,25 +154,30 @@ export default function EditProfile() {
             <input
               type="date"
               id="dob"
-              name="dob"
+              name="dateOfBirth"
+              value={formData?.dateOfBirth}
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              onChange={handleChange}
             />
           </div>
           <div className="mb-4">
             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
-              Email
+              LinkedIn Profile
             </label>
             <input
               type="email"
               id="email"
-              name="email"
+              name="linkedIn"
+              value={formData.linkedIn}
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              onChange={handleChange}
             />
           </div>
           <div className="flex items-center justify-between">
             <button
               type="submit"
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              onClick={handleSubmit}
             >
               Save Changes
             </button>
