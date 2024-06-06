edeea1142b2cb480b42c9fee97e9c8f3ec8c896f
ViveK Poonia <vivekpoonia2112@gmail.com>
2024-06-06 09:42:50 +0530

    opp_card change

--- client/src/components/Card_opp.jsx
+++ client/src/components/Card_opp.jsx
@@ -1,14 +1,7 @@
-import {
-  Card,
-  CardBody,
-  CardFooter,
-  Typography,
-  Button,
-} from "@material-tailwind/react";
 import { Link } from "react-router-dom";
 import { IoIosHeartEmpty } from "react-icons/io";
 import { MdDeleteOutline } from "react-icons/md";
-import { FaHeart } from "react-icons/fa6";
+import { FaHeart } from "react-icons/fa";
 import { sanity } from "../sanity";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
@@ -56,10 +49,10 @@ export default function CardItem({ oppertunity, onUpdate }) {
     sanity
       .delete(deleteQuery)
       .then((response) => {
-        console.log("Achievement deleted successfully:", response);
+        console.log("Oppertunity deleted successfully:", response);
       })
       .catch((error) => {
-        console.error("Error deleting achievement:", error);
+        console.error("Error deleting oppertunity:", error);
       });
   };
 
@@ -71,46 +64,50 @@ export default function CardItem({ oppertunity, onUpdate }) {
   }, [oppertunity, currentUser]);
 
   return (
-    <Card className="mt-6 w-80 shadow-md rounded-md bg-slate-100 p-2">
-      <CardBody>
-        <div className="flex justify-between">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            className="mb-4 h-12 w-12 text-gray-900"
-          >
-            <path
-              fillRule="evenodd"
-              d="M9.315 7.584C12.195 3.883 16.695 1.5 21.75 1.5a.75.75 0 01.75.75c0 5.056-2.383 9.555-6.084 12.436A6.75 6.75 0 019.75 22.5a.75.75 0 01-.75-.75v-4.131A15.838 15.838 0 016.382 15H2.25a.75.75 0 01-.75-.75 6.75 6.75 0 017.815-6.666zM15 6.75a2.25 2.25 0 100 4.5 2.25 2.25 0 000-4.5z"
-              clipRule="evenodd"
-            />
-            <path d="M5.26 17.242a.75.75 0 10-.897-1.203 5.243 5.243 0 00-2.05 5.022.75.75 0 00.625.627 5.243 5.243 0 005.022-2.051.75.75 0 10-1.202-.897 3.744 3.744 0 01-3.008 1.51c0-1.23.592-2.323 1.51-3.008z" />
-          </svg>
-          <div style={{ display: 'flex', alignItems: 'center', gap: '4px' }}>
-            <h >{likeCount}</h>
-            <button onClick={handleLike}>
-              {likeToggle ? <FaHeart style={{ fontSize: "2em" }} /> : <IoIosHeartEmpty style={{ fontSize: "2em" }} />}
-            </button>
-          </div>
+    <div className="mt-6 w-80 shadow-md rounded-md bg-slate-100 p-4">
+      <div className="flex justify-between">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          className="mb-4 h-12 w-12 text-gray-900"
+        >
+          <path
+            fillRule="evenodd"
+            d="M9.315 7.584C12.195 3.883 16.695 1.5 21.75 1.5a.75.75 0 01.75.75c0 5.056-2.383 9.555-6.084 12.436A6.75 6.75 0 019.75 22.5a.75.75 0 01-.75-.75v-4.131A15.838 15.838 0 016.382 15H2.25a.75.75 0 01-.75-.75 6.75 6.75 0 017.815-6.666zM15 6.75a2.25 2.25 0 100 4.5 2.25 2.25 0 000-4.5z"
+            clipRule="evenodd"
+          />
+          <path d="M5.26 17.242a.75.75 0 10-.897-1.203 5.243 5.243 0 00-2.05 5.022.75.75 0 00.625.627 5.243 5.243 0 005.022-2.051.75.75 0 10-1.202-.897 3.744 3.744 0 01-3.008 1.51c0-1.23.592-2.323 1.51-3.008z" />
+        </svg>
+        <div className="flex items-center gap-2">
+          <h3>{likeCount}</h3>
+          <button onClick={handleLike}>
+            {likeToggle ? (
+              <FaHeart className="text-red-500 text-2xl" />
+            ) : (
+              <IoIosHeartEmpty className="text-2xl" />
+            )}
+          </button>
         </div>
-        <Typography variant="h5" color="blue-gray" className="mb-2 mx-5 font-semibold uppercase">
-          {oppertunity?.title}
-        </Typography>
-        <Typography className="mb-2 mx-5 hover:no-underline">
-          <b>Posted for: </b> {oppertunity?.participants}
-        </Typography>
-        <Typography className="mb-2 mx-5 hover:no-underline">
-          <b>Apply Link: </b> {oppertunity?.applylink}
-        </Typography>
-        <Typography className="mb-2 mx-5 hover:no-underline">
-          <b>Date: </b> {oppertunity?.openingdate} : {oppertunity?.closingdate}
-        </Typography>
-        <Typography className="mb-2 mx-5 hover:no-underline"><b>Description: </b>{oppertunity?.description}</Typography>
-      </CardBody>
-      <CardFooter className="pt-0 flex justify-between mx-5">
+      </div>
+      <h5 className="mb-2 mx-5 font-semibold uppercase text-blue-gray-700">
+        {oppertunity?.title}
+      </h5>
+      <p className="mb-2 mx-5">
+        <b>Posted for: </b> {oppertunity?.participants}
+      </p>
+      <p className="mb-2 mx-5">
+        <b>Apply Link: </b> {oppertunity?.applylink}
+      </p>
+      <p className="mb-2 mx-5">
+        <b>Date: </b> {oppertunity?.openingdate} : {oppertunity?.closingdate}
+      </p>
+      <p className="mb-2 mx-5">
+        <b>Description: </b>{oppertunity?.description}
+      </p>
+      <div className="pt-0 flex justify-between mx-5">
         <Link to={`/Opp_card_details/${oppertunity?._id}`}>
-          <Button size="sm" variant="text" className="flex items-center gap-2">
+          <button className="flex items-center gap-2 text-blue-500">
             Learn More
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -126,12 +123,12 @@ export default function CardItem({ oppertunity, onUpdate }) {
                 d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
               />
             </svg>
-          </Button>
+          </button>
         </Link>
         <button onClick={handleDelete}>
-          <MdDeleteOutline style={{ fontSize: "2em" }} />
+          <MdDeleteOutline className="text-2xl text-red-500" />
         </button>
-      </CardFooter>
-    </Card>
+      </div>
+    </div>
   );
 }
